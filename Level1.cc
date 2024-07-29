7c713914ffc9feec4f56f15f8cf727671bcb85d2
hudsonkoyanagi <hudsonkoyanagi@gmail.com>
2024-07-29 12:24:54 -0400

    kind of working bots

--- Level1.cc
+++ Level1.cc
@@ -14,12 +14,15 @@ MOVE_RESULTS Level1::make_move(bool white_to_move) {
         for (int j = 0;j < 8;++j) {
             Piece* currPiece = model->board[i][j];
             if (currPiece->col == WHITE && white_to_move) {
-                for (int m = 0;m < 8;++m) {
+                for (int m = 0; m < 8;++m) {
                     for (int n = 0;n < 8;++n) {
                         std::cout << "";
                         MOVE_RESULTS currMove = model->is_valid(Move{ Cord{i,j},Cord{m,n} }, white_to_move);
+
                         if (currMove != INVALID_MOVE) {
-                            valid_moves.push_back(Move{ Cord{i,j},Cord{m,n} });
+                            Move mv{ Cord{i,j}, Cord{m,n} };
+                            mv.move_result = currMove;
+                            valid_moves.push_back(mv);
                         }
                     }
                 }
