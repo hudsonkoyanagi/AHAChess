0de16822238b79062d11e9b73ac3e62694b495dc
hudsonkoyanagi <hudsonkoyanagi@gmail.com>
2024-07-29 17:00:07 -0400

    added print to move

--- Move.cc
+++ Move.cc
@@ -1,4 +1,5 @@
 #include "Move.h"
+#include "utils.h"
 #include <iostream>
 
 Move::Move(Cord s, Cord e) : start{ s }, end{ e }, moved{ EMPTY }, taken{ EMPTY }, move_result{ INVALID_MOVE }, check{ false } {}
@@ -9,4 +10,7 @@ Move::Move(int sr, int sc, int er, int ec) : start{ Cord{sr, sc} }, end{ Cord{er
 void Move::print() {
     std::cout << "start: <" << start.row << ", " << start.col << ">" << std::endl;
     std::cout << "end: <" << end.row << ", " << end.col << ">" << std::endl;
+    printMoveResult(move_result);
+    printPiece(moved);
+    printPiece(taken);
 }
